8280b71014f0b7bb04d6cf33891cba7c55a889e0
TheMiranha <lxbrmiranda@gmail.com>
2026-08-07 22:10:11 -0300

    first commit

---
+++ src/main/java/com/miranda/opencord/OpencordApplication.java
@@ -0,0 +1,13 @@
+package com.miranda.opencord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencordApplication.class, args);
+    }
+
+}
